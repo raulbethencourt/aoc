a9586a155b1cc7f74efe35216ac2a60561faccf5
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-12-04 22:30:47 +0100

    feat(day1): working in sorting

--- 2024/day1.c
+++ 2024/day1.c
@@ -1,31 +1,51 @@
 #include "day1.h"
+#include <stdio.h>
 
-int main(void) {
-    FILE *fp;
+int countLinesInFile(FILE *fp) {
     int count = 0;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char c;
 
-    fp = fopen("day1.test", "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
-
-    for (c = getc(fp); c != EOF; c = getc(fp))
+    for (char c = getc(fp); c != EOF; c = getc(fp))
         if (c == '\n')
             count = count + 1;
 
+    return count;
+}
 
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("%s", line);
+int sortNumbers(int count) {
+    int array[count], index, i, j, min, temp;
+
+    for (i = 0; i < count - 1; i++) {
+        min = array[i];
+        for (j = i + 1; j < count; j++)
+            if (array[j] < min) {
+                min = array[j];
+                index = j;
+            }
+
+        temp = array[i];
+        array[i] = min;
+        array[index] = temp;
     }
 
+    return array[count];
+}
+
+int main(void) {
+    FILE *fp = fopen("day1.test", "r");
+
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    int count = countLinesInFile(fp);
+    int array[count];
+    array = sortNumbers(count);
+
     fclose(fp);
-    
-    printf("%d", count);
-    if (line)
-        free(line);
+
+    for (int i = 0; i < count; i++)
+        printf("%d\n", array[i]);
+
+    printf("%d\n", count);
 
     return 11;
 }
