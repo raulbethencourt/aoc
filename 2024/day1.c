367f06a7e704e841dff930cf2ea2b4c1c7efaabf
raulbethencourt <raul.bethencourt.pro@gmail.com>
2024-12-08 22:50:24 +0100

    feat(main): start to read lines

--- 2024/day1.c
+++ 2024/day1.c
@@ -1,24 +1,30 @@
 #include "day1.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    FILE* fp = fopen("day1.test", "r");
+    FILE* stream = fopen("day1.test", "r");
     char* line = NULL;
     size_t len = 0;
     ssize_t read;
 
-    if (fp == NULL)
+    if (stream == NULL) {
+        perror("fopen");
         exit(EXIT_FAILURE);
-
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("%s", line);
     }
 
-    fclose(fp);
-    if (line)
-        free(line);
+    while ((read = getline(&line, &len, stream)) != -1) {
+        int i = 0;
+        do {
+            if (!isspace(line[i]))
+                printf("%c", line[i]);
+            i++;
+        } while (line[i] != '\0');
+    }
 
-    return 11;
+    free(line);
+    fclose(stream);
+    exit(EXIT_SUCCESS);
 }
